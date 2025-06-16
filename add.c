a6a12b569f7288c8bb67bf79721e6767a85f41af
unknown <nikhilsabale7716@gmail.com>
2025-06-16 13:29:04 +0530

    Addition

---
+++ add.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+int main(){
+    int a,b;
+
+    printf("Enter ANy two no :- ");
+    scanf("%d%d",&a,&b);
+
+    printf("The addition of two no is :- %d ",a+b);
+    return 0;
+}
